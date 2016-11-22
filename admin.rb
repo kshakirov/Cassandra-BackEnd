d0ea331ed7b58f584807b1e270db72c19e70bbc8
kshakirov <kshakirov@zoral.com.ua>
2016-11-22 18:14:01 +0200

    WIP #19 attribute attribute sets started

--- admin.rb
+++ admin.rb
@@ -8,6 +8,8 @@ class Admin < Sinatra::Base
     set :productBackEnd, TurboCassandra::ProductBackEnd.new
     set :loginBackEnd, TurboCassandra::Login.new
     set :orderBackEnd, TurboCassandra::OrderBackEnd.new
+    set :attributeBackEnd, TurboCassandra::AttributeBackEnd.new
+    set :attributeSetBackEnd, TurboCassandra::AttributeSetBackEnd.new
   end
 
 
@@ -23,4 +25,22 @@ class Admin < Sinatra::Base
   get '/customer/:id' do
     settings.customerBackEnd.get_customer_info(params['id'].to_i)
   end
+
+  get '/attribute/' do
+    settings.attributeBackEnd.get_attributes_list
+  end
+
+  get '/attribute_set/' do
+    settings.attributeSetBackEnd.get_attribute_sets_list
+  end
+
+  get '/attribute_set/:code' do
+    settings.attributeSetBackEnd.get_attribute_set('code', params[:code])
+  end
+
+  post '/attribute_set/:code' do
+    request_payload = JSON.parse request.body.read
+    settings.attributeSetBackEnd.update_critical_property(request_payload)
+  end
+
 end
