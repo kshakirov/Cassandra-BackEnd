c6b197149307a46659fab3589c977a9c10871fad
kshakirov <kshakirov@zoral.com.ua>
2017-02-28 12:50:24 +0200

    WIP #30 products tipar done

--- lib/tools/BackEnd/product/controller/product.rb
+++ lib/tools/BackEnd/product/controller/product.rb
@@ -1,7 +1,9 @@
 module TurboCassandra
   module Controller
     class Product
+      attr_accessor :product_api
       include TurboCassandra::Controller::CriticalDims
+      include TurboCassandra::Controller::TiPart
       public
       def initialize
         @product_api = TurboCassandra::API::Product.new
@@ -10,11 +12,18 @@ module TurboCassandra
 
       private
 
+      def is_ti_product?  product
+        product['manufacturer'] == 'Turbo International'
+      end
+
       def _get_product sku
         sku = sku.to_i
         product = @product_api.find_by_sku(sku).first
         if product.key? 'critical_decimal' and not product['critical_decimal'].nil?
           product['critical'] = add_critical_attributes(product)
+        end
+        unless  is_ti_product? product
+          product['ti_part_sku'], product['ti_part_number'] = add_ti_part_attributes(product)
         end
         product
       end
