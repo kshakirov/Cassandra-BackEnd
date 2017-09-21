5e1c97a1a57c812f33dc42de4c1a7179172fd4ac
Kirill Shakirov <kshakirov@zoral.com.ua>
2017-09-21 11:41:27 +0000

    merged

--- tools/sync/product/console/product_sync_all.rb
+++ tools/sync/product/console/product_sync_all.rb
@@ -2,7 +2,7 @@ require_relative '../../sync_helper'
 metadata_server = get_metdata_host
 updater = TurboCassandra::Sync::Product::Rest.new(metadata_server)
 
-(0..720).to_a.each  do |time|
+(0..730).to_a.each  do |time|
   start_time = Time.now
   ids = ((time *100)..(time * 100 + 99)).to_a
   updater.update_specific_array(ids)
